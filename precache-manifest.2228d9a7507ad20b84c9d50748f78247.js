76e5ccf8da0756a5db0c000810ea99a989a258ca
恋仔 <1842563969@qq.com>
2020-10-06 17:55:45 +0800

    deploy

---
+++ precache-manifest.2228d9a7507ad20b84c9d50748f78247.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6240d24d7bba8aeea806",
+    "url": "/morney-website/css/app.8c838731.css"
+  },
+  {
+    "revision": "3411275b170c279b4f7ebb3c3e7a12ca",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "6240d24d7bba8aeea806",
+    "url": "/morney-website/js/app.12f93388.js"
+  },
+  {
+    "revision": "047942fef17c2adb05fe",
+    "url": "/morney-website/js/chunk-vendors.a720af33.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "9b43ffb041684f6e0778bd1fa11b7bb5",
+    "url": "/morney-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
